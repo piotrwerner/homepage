0c4b2c10c3104eb91e86a3fad1c1f70b4b77fead
Piotr Werner <piotr.wernerr@gmail.com>
2019-06-11 06:02:55 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('siema')
+const name = 'Piotrek';
+const age = 30;
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+console.log('Witaj, przybyszu!');
