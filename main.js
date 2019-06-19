734c86c706cedc55db70a68a0d846643db91298d
piotrwerner <33878284+piotrwerner@users.noreply.github.com>
2019-06-19 06:27:52 +0200

    Add files via upload
    bem, simplified css, js functions

--- main.js
+++ main.js
@@ -1,9 +1,54 @@
-console.log('siema')
-const name = 'Piotrek';
-const age = 30;
+console.log("siema");
+const name = "Piotrek";
+const age = 24;
+let piotrek = null;
 console.log(name);
 console.log(age);
 
 console.log(`Nazywam się ${name} i mam ${age} lat`);
 
-console.log('Witaj, przybyszu!');
+console.log("Witaj, przybyszu!");
+
+const omnie = document.querySelector(".about__paragraph--js");
+omnie.innerHTML = `Nazywam się ${name} i mam ${age} lat`;
+
+if ("javascript" != "java") {
+  console.log("to prawda!");
+}
+
+if (age < 20) {
+  console.log("Dzieciak jesteś.");
+} else if (age > 20 && age <= 29) {
+  console.log("Jesteś dwudziestokilkulatkiem.");
+} else {
+  console.log("Jesteś 30+.");
+}
+
+switch (age) {
+  case 20:
+    console.log(`masz równo 20 lat`);
+    break;
+  case 30:
+    console.log(`masz równo 30 lat`);
+    break;
+  default:
+    console.log(`masz ${age} lat`);
+    break;
+}
+
+let oldIndicator;
+
+const amIOld = age > 70 ? `yes` : `no`;
+console.log(amIOld);
+
+function calculate(x) {
+  x = x + 3;
+  console.log(x);
+  return x*7;
+}
+
+console.log(calculate(2));
+
+const calculateFat = x => (x+3)*7;
+
+console.log(calculateFat(3));
